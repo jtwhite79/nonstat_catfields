ad1c2b98bec326dfd912d21ef5eb9618ebfb4319
jwhite <jtwhite1000@gmail.com>
2024-12-08 09:47:49 -0500

    more

--- workflow.py
+++ workflow.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import flopy 
 import pyemu
 
-
+is_arm = False
 if sys.platform.startswith('win'):
     bin_path = os.path.join("bin","win")
     
@@ -17,6 +17,7 @@ elif sys.platform.startswith('linux'):
 
 elif sys.platform.lower().startswith('dar') or sys.platform.lower().startswith('mac'):
     bin_path = os.path.join("bin","mac")
+    is_arm = True
     
 else:
     raise Exception('***ERROR: OPERATING SYSTEM UNKOWN***')
@@ -31,7 +32,7 @@ def build_flowmodel(new_d):
                                  memory_print_option="ALL",continue_=True)
 
     nper = 1
-    sp_len = 1000000
+    sp_len = 1000000000
     tdis_pd = [(sp_len, 1, 1.0) for _ in range(nper)]
     tdis = flopy.mf6.ModflowTdis(sim, pname="tdis", time_units="DAYS", nper=nper, perioddata=tdis_pd)
 
@@ -124,9 +125,7 @@ def quick_domain_plot(ws):
     gwf = sim.get_model()
     hds = flopy.utils.HeadFile(os.path.join(ws,"model.hds"),model=gwf)
     totim = np.cumsum(sim.tdis.perioddata.array["perlen"])
-    df = load_pathline(os.path.join(ws,'model_mp.mppth'),totim)
-    print(df.columns)
-    exit()
+    df = load_pathline(os.path.join(ws,'model_mp.mppth'))
 
     x = gwf.modelgrid.xcellcenters
     y = gwf.modelgrid.ycellcenters
@@ -142,12 +141,32 @@ def quick_domain_plot(ws):
         pdf = df.loc[df.particleid==pid,:].copy()
         pdf.sort_values(by="ptime",inplace=True)
         ax.plot(pdf.x,pdf.y,"k--")
+        #print(pdf.iloc[-1,:])
 
     plt.savefig(os.path.join(ws,"quick_domain_plot.pdf"))
     plt.close(fig)
 
 
-def load_pathline(pathline_fname,totim):
+def get_final_particle_info(pathline_fname):
+    if isinstance(pathline_fname,str):
+        df = load_pathline(pathline_fname)
+    else:
+        df = pathline_fname
+    pids = df.particleid.unique()
+    pids.sort()
+    final_time,final_x = [],[]
+    for pid in pids:
+        pdf = df.loc[df.particleid==pid,:].copy()
+        pdf.sort_values(by="ptime",inplace=True)
+        pdf = pdf.iloc[-1,:]
+        final_time.append(pdf.ptime)
+        final_x.append(pdf.x)
+    results = pd.DataFrame({"ptime":final_time,"x":final_x},index=pids)
+    results.index.name = "pid"
+    return results
+
+
+def load_pathline(pathline_fname="model_mp.mppth"):
     dfs = []
     pids = []
     with open(pathline_fname, 'r') as f:
@@ -157,7 +176,6 @@ def load_pathline(pathline_fname,totim):
             line = f.readline()
             if line == "":
                 break
-
             hraw = line.strip().split()
             pid = int(hraw[2])
             nrec = int(hraw[3])
@@ -183,25 +201,119 @@ def load_pathline(pathline_fname,totim):
                 data["layer"].append(layer)
                 data["ptime"].append(t)
             df1 = pd.DataFrame(data, index=data["ptime"])
-
             df1.drop_duplicates(inplace=True, subset="ptime")
             # print(df)
             pids.append(pid)
-            df = df1#.reindex(totim, fill_value=np.nan)
-            # if df.shape[0] != totim.shape[0]:
-            #print(pid)
-
+            df = df1
             df.loc[:, "particleid"] = pid
-            #df.loc[:, "ptime"] = totim
             dfs.append(df.copy())
-
     dfmp7 = pd.concat(dfs, axis=0, ignore_index=True)
+    dfmp7["particleid"] = dfmp7.particleid.astype(int)
     #dfmp7.loc[:,"status"] = dfmp7.particleid.apply(lambda x: status[x])
+    dfmp7.index.name = "count"
     return dfmp7
 
 
+def interp_pathline_to_consistent_nobs(pathline_fname):
+    if isinstance(pathline_fname,str):
+        df = load_pathline(pathline_fname)
+    else:
+        df = pathline_fname
+    results = get_final_particle_info(df)
+    pids = df.particleid.unique()
+    pids.sort()
+    reidx = []
+    for pid in pids:
+        pdf = df.loc[df.particleid==pid,:].copy()
+        maxtime = pdf.loc[pdf.y!=0,'ptime'].max()
+        times = np.linspace(0,maxtime,100)
+        pdf.index = pdf.ptime
+        rdf = pdf.reindex(times,method="nearest")
+        rdf["particleid"] = pdf.particleid.values[0]
+        reidx.append(rdf)
+    reidx = pd.concat(reidx)
+    reidx.pop("ptime")
+    return reidx
+
+
+def post_process_run(ws="."):
+    hds = flopy.utils.HeadFile(os.path.join(ws,"model.hds"))
+    arr = hds.get_data()[0,:,:]
+    np.savetxt(os.path.join(ws,"heads.dat"),arr,fmt="%15.6E")
+    pathdf = load_pathline(os.path.join(ws,'model_mp.mppth'))
+    resultsdf = get_final_particle_info(pathdf)
+    reidx = interp_pathline_to_consistent_nobs(pathdf)
+    reidx.to_csv(os.path.join(ws,"pathline_consistent.csv"))
+    pathdf.to_csv(os.path.join(ws,"pathline.csv"))
+    resultsdf.to_csv(os.path.join(ws,"endpoint.csv"))
+    return arr,pathdf,resultsdf
+
+
+def setup_pst(org_d):
+    temp_d = 'temp'
+    if os.path.exists(os.path.join(temp_d)):
+        shutil.rmtree(temp_d)
+    shutil.copytree(org_d,temp_d)
+    shutil.copytree("pyemu",os.path.join(temp_d,"pyemu"))
+    if is_arm:
+        ppu_d = "pypestutils"
+        if os.path.exists(ppu_d):
+            shutil.rmtree(ppu_d)
+        shutil.copytree(os.path.join("..",ppu_d,ppu_d),ppu_d)
+        shutil.copytree(os.path.join("..",ppu_d,ppu_d),os.path.join(temp_d,ppu_d))
+        import sys
+        sys.path.insert(0,".")
+    import pypestutils as ppu
+        
+    sim = flopy.mf6.MFSimulation.load(sim_ws=temp_d)
+    gwf = sim.get_model()
+    karr_fname = "model.npf_k.txt"
+    arr = np.loadtxt(os.path.join(temp_d,karr_fname)).reshape(gwf.dis.nrow.data,gwf.dis.ncol.data)
+    np.savetxt(os.path.join(temp_d,karr_fname),arr,fmt="%15.6E")
+    pyemu.os_utils.run("mf6",cwd=temp_d)
+    pyemu.os_utils.run("mp7 model_mp.mpsim",cwd=temp_d)
+    arr,pathdf,resultsdf = post_process_run(ws=temp_d)
+    
+    pf = pyemu.utils.PstFrom(temp_d,"template",spatial_reference=gwf.modelgrid,remove_existing=True)
+
+    pf.extra_py_imports.append("shutil")
+    pf.extra_py_imports.append("os")
+    pf.extra_py_imports.append("flopy")
+
+    pf.mod_sys_cmds.append("mf6")
+    pf.mod_sys_cmds.append("mp7 model_mp.mpsim")
+
+    pf.add_py_function("workflow.py","interp_pathline_to_consistent_nobs(ws='.')",is_pre_cmd=None)
+    pf.add_py_function("workflow.py","get_final_particle_info(pathdf)",is_pre_cmd=None)
+    pf.add_py_function("workflow.py","load_pathline(pathdf)",is_pre_cmd=None)
+    pf.add_py_function("workflow.py","post_process_run()",is_pre_cmd=False)
+    
+    
+    pf.add_observations("pathline_consistent.csv",index_cols=["particleid","ptime"],
+                        use_cols=["x","y"],prefix="pathline",obsgp="pathline")
+    pf.add_observations("endpoint.csv",index_cols=["pid"],
+                        use_cols=["ptime","x"],prefix="endpoint",obsgp="endpoint")
+    pf.add_observations("heads.dat",prefix="head",obsgp="head")
+
+
+    pf.add_observations(karr_fname,prefix="karr",obsgp="karr")
+
+    v = pyemu.geostats.ExpVario(contribution=1.0,a=200)
+    gs = pyemu.geostats.GeoStruct(variograms=v)
+    pf.add_parameters(karr_fname,par_type="pilotpoints",geostruct=gs,pargp="pp",
+                      upper_bound=10.0,lower_bound=0.1,
+                      pp_options={"pp_space":30,"prep_hyperpars":True,"try_use_ppu":True})
+
+    pst = pf.build_pst(version=None)
+    pst.control_data.noptmax = 0
+    pst.write(os.path.join(pf.new_d,"pest.pst"),version=2)
+    pyemu.os_utils.run("pestpp-ies pest.pst",cwd=pf.new_d)
+
 
 if __name__ == "__main__":
-    build_flowmodel("base_model")
-    build_ptmodel("base_model")
-    quick_domain_plot("base_model_pt")
+    #build_flowmodel("base_model")
+    #build_ptmodel("base_model")
+    #quick_domain_plot("base_model_pt")
+    #post_process_run("base_model_pt")
+    setup_pst("base_model_pt")
+    #interp_pathline_to_consistent_nobs(os.path.join("temp",'model_mp.mppth'))
